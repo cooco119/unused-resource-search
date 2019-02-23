71abb406a56f889c051d21a06fb4a092cfa222c0
Ubuntu <ubuntu@ip-172-31-20-109.ap-northeast-2.compute.internal>
2019-02-23 05:20:37 +0000

    initial commit

--- FormRadixTree.py
+++ FormRadixTree.py
@@ -1,1 +1,12 @@
-import py-radix
+import radix
+import urllib
+from datetime import date
+
+## Fetch today data
+
+today = date.today()
+year = today.year
+month = today.month
+day = today.day
+
+urllib.request()
